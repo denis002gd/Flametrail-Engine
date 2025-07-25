62bbf2db65069e6519101ad3306333786235f835
Denis <pisarencodenis830@gmail.com>
2025-07-26 02:29:02 +0300

    InitialCommit
    just some file declarations

---
+++ render.h
@@ -0,0 +1,18 @@
+#ifndef RENDER_H
+#define RENDER_H
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_video.h>
+
+typedef struct Game{
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  
+  double deltaTime;
+  int running;
+}GameContext;
+
+int InitGameContext(GameContext *Game_Context);
+void FreeGameContext(GameContext *Game_Context);
+
+#endif // !RENDER_H
